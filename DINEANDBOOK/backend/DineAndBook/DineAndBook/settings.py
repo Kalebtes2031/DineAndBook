f9ddfffa818f3d6f4350c7183985becd69256dd5
Kalebtes2031 <kalebtesfaye2031@gmail.com>
2024-07-31 14:03:52 +0300

    allowed vercel.app in the allowed hosts list

--- DINEANDBOOK/backend/DineAndBook/DineAndBook/settings.py
+++ DINEANDBOOK/backend/DineAndBook/DineAndBook/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'django-insecure-ipvt35*_9k5jt#$5sv$^5(9*r&7v#m#h*m=tdsbfr!-*hqzt)8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app","127.0.0.1","localhost",".now.sh"]
 
 
 # Application definition
